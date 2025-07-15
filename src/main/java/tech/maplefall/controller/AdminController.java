8c21b77af0f9ec7b299c89bb54668cab8088c289
Maplefall2377 <1395848853@qq.com>
2025-07-15 15:46:44 +0800

    完成管理员登出操作，登入时记录token，登出时在localstorage处移除token，前面的操作未知是否正确,未完成管理员密码修改功能,7月13日上午任务

--- src/main/java/tech/maplefall/controller/AdminController.java
+++ src/main/java/tech/maplefall/controller/AdminController.java
@@ -1,15 +1,19 @@
 package tech.maplefall.controller;
 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.maplefall.entity.Admin;
 import tech.maplefall.service.IAdminService;
+import tech.maplefall.util.RedisUtils;
 import tech.maplefall.util.Result;
 import tech.maplefall.util.SafeUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Slf4j
 @RequestMapping("/api/admin")
@@ -18,6 +22,12 @@ public class AdminController {
 
     @Autowired
     private IAdminService adminService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private RedisUtils redisUtils;
 
     //管理员登录
     @PostMapping("/login")
@@ -37,7 +47,24 @@ public class AdminController {
             return Result.error("账号或密码有误");
         }
 
-        return Result.success("登录成功");
+        //生成用户令牌，作为用户身份，id拼接username，然后进行加密，加密后的串作为令牌
+        String token = SafeUtils.encode(admin2.getId() + admin2.getUsername());
+        //将令牌存入redis中,key是token，value是user对应的map
+        Map<String, Object> adminMap = objectMapper.convertValue(admin2, new TypeReference<Map<String, Object>>() {});
+        redisUtils.hmset(token, adminMap, 7200); // 设置过期时间为2小时
+
+        return Result.success("登录成功",token);
+    }
+
+    //管理员登出
+    @GetMapping("/logout/{id}")
+    public Result logout(HttpServletRequest request) {
+        String token = request.getHeader("token-admin");
+//        if (token == null || token.isEmpty()) {
+//            return Result.error("未登录或令牌无效");
+//        }
+        redisUtils.del(token); // 删除令牌
+        return Result.success("登出成功");
     }
 }
 
